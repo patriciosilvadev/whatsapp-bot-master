0a33af5f6f6a7c33a06dc5494450f59d83b90152
iuha <cacjorge@gmail.com>
2020-12-13 18:17:14 -0300

    Revert "Revert "adding time again""
    This reverts commit 878cda8d70776d08ba7cd28719bc91bdaf6d7e12.

--- msgHndlr.js
+++ msgHndlr.js
@@ -2,6 +2,7 @@ const { decryptMedia } = require('@open-wa/wa-decrypt')
 const fs = require('fs-extra')
 const get = require('got')
 const fetch = require('node-fetch')
+const moment = require('moment-timezone')
 const color = require('./lib/color')
 const { spawn, exec } = require('child_process')
 const { liriklagu, quotemaker, randomNimek, fb, sleep, jadwalTv, ss } = require('./lib/functions')
@@ -51,6 +52,7 @@ module.exports = msgHandler = async (client, message) => {
                 Iv: '[❗] The link you submitted is invalid!'
             }
         }
+		const time = moment(t * 1000).format('DD/MM HH:mm:ss')
         const botNumber = await client.getHostNumber()
         const blockNumber = await client.getBlockedIds()
         const groupId = isGroupMsg ? chat.groupMetadata.id : ''
@@ -62,7 +64,7 @@ module.exports = msgHandler = async (client, message) => {
         const isBlocked = blockNumber.includes(sender.id)
 		const bakaNumber = ['556281313310@c.us']
 		const isBaka = bakaNumber.includes(sender.id)
-        const isNsfw = isGroupMsg ? nsfw_.includes(chat.id) : false
+        //const isNsfw = isGroupMsg ? nsfw_.includes(chat.id) : false
         const uaOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
         const isUrl = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi)
         if (!isGroupMsg && command.startsWith('!')) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(msgs(command)), 'from', color(pushname))
